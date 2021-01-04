5d3b1a03b03b64e11112e2858af00c160f12bb36
nitinbhosale <bhosalenb@gmail.com>
2021-01-04 21:40:01 +0530

    Program to print star patterns

---
+++ StarPattern.py
@@ -0,0 +1,27 @@
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+
+def starPattern(n):
+    # outer loop to handle number of rows
+    # n in this case
+    for i in range(0, n):
+
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i + 1):
+            # printing stars
+            print("* ", end="")
+
+        # ending line after each row
+        print("\r")
+
+    # Driver Code
+
+
+n = int(input("Enter no. of rows want to print: "))
+starPattern(n)
