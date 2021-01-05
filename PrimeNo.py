86cc2ce17536b03d3accd034eb67ab75369f8aab
nitinbhosale <bhosalenb@gmail.com>
2021-01-05 19:27:43 +0530

    List of prime nos between 1 to 100

--- PrimeNo.py
+++ PrimeNo.py
@@ -1,15 +1,24 @@
-# check if a number is prime or not
+# List of prime nos between 1 to 100:
 
-num = int(input("Enter a number: "))
-
-if num > 1:
-
-    for i in range(2, num):
-        if (num % i) == 0:
-            print(num, "is not a prime number")
-            break
+def primeNo(n):
+    flag=0
+    if n <=1:
+        return None
     else:
-        print(num, "is a prime number")
 
-else:
-    print(num, "is not a prime number")
+        for i in range(2, int(n/2)):
+            if n % i == 0:
+                flag=0
+                break
+        else:
+                flag = 1
+        if flag ==0:
+            return None
+        else:
+            return n
+
+print("List of prime nos between 1 to 100:")
+for j in range(1,101):
+    pn = primeNo(j)
+    if pn is not None:
+        print(pn)
