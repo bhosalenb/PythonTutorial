d0d014c96c4c3e0524cc5674e98a83b11a1d513c
nitinbhosale <bhosalenb@gmail.com>
2021-01-06 21:48:58 +0530

    Demo program for Special Vairable

---
+++ Demo_Special_Vairable/Calc.py
@@ -0,0 +1,15 @@
+def add():
+    print("Addition of nums")
+
+def sub():
+    print("Substraction of nums")
+
+def mult():
+    print("Multiplication of nums")
+
+def div():
+    print("Division of 2 nums")
+
+
+add()
+
