407bdede3929a61662836871962a3bd865342cc7
nitinbhosale <bhosalenb@gmail.com>
2021-01-07 16:30:16 +0530

    Demo Multi level inheritance

--- Demo_Inheritance.py
+++ Demo_Inheritance.py
@@ -1,18 +1,23 @@
 # Multi level inheritance
 
 class A:
+    def __init__(self):
+        print("I am class A constructor")
     def feature1(self):
         print("I am in Feature1")
     def feature2(self):
         print("I am in Feature2")
 
 class B(A):
+    def __init__(self):
+        print("I am class B constructor")
     def feature3(self):
         print("I am in Feature3")
     def feature4(self):
         print("I am in Feature4")
 
 class C(B):
+
     def feature5(self):
         print("I am in Feature5")
     def feature6(self):
