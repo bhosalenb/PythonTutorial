bea67a2b82f66afa37764c70d8f7749a7d4f5780
nitinbhosale <bhosalenb@gmail.com>
2021-01-08 04:22:35 +0530

    Students Program using OPPS concept

--- Students.py
+++ Students.py
@@ -16,7 +16,7 @@ class Student:
     def info(cls):
         return cls.school
 
-    #def sortAge(self):
+    ##def sortAge(self):
 
 s1 = Student('Nitin',34,[91,94,97])
 s2 = Student('Jaysing',39,[96,93,99])
