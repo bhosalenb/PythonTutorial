e2538a297f141ae019612330e9666b58be2a2390
nitinbhosale <bhosalenb@gmail.com>
2021-01-09 07:45:14 +0530

    Binary search program

---
+++ Searching/BinarySearch.py
@@ -0,0 +1,23 @@
+# FOr binary search, list should be already sorted
+def bSearch(list,n):
+    l = 0
+    u = len(list)-1
+
+    while l<=u:
+        mid = l + u
+        if list[mid] == n:
+            return True
+        elif list[mid] < n:
+            l = mid
+        else:
+            u = mid
+
+
+list = [2,12,13,15,23,25,33,43]
+######,,0,,1,,2,,3,,4,,5,,6,,7
+n=33
+if bSearch(list,n):
+    print("Found")
+
+else:
+    print("not Found")
