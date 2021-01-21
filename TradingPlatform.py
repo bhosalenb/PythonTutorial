f381d8686e4bfe2af7e052ab491ae57e9f4d33f7
nitinbhosale <bhosalenb@gmail.com>
2021-01-21 16:33:15 +0530

    Order matching engine

--- TradingPlatform.py
+++ TradingPlatform.py
@@ -1,24 +1,20 @@
 class Order:
     def __init__(self,lst1):
+        #N,1,TCS,S,32,108
+        #0,1,,2,,3,4,,,5
         self.Command = lst1[0]
-        self.Sym = lst1[1]
-        self.Side = lst1[2]
-        self.Price = lst1[4]
-        self.Qty = lst1[3]
+        self.ordId=lst1[1]
+        self.Sym = lst1[2]
+        self.Side = lst1[3]
+        self.Qty = lst1[4]
+        self.Price = lst1[5]
 
     def new(self):
-        # N,SYM,Side,Price, Qty
         print(type(self.Sym))
-        #if self.Sym !=
 
-
-    def amend(self):
-        pass
-    def cancel(self):
-        pass
-    def match(self):
-        pass
     def showBuy(self):
+        if self.Side == 'B':
+            print()
         print(self.Sym,',',self.Side,',',self.Qty,',',self.Price,end='')
     def showSell(self):
         print('--',self.Price,',',self.Qty,',',self.Side)
@@ -36,47 +32,72 @@ def query(buy_list, sell_list):
     else:
         maxLength=len(buy_list)
     for j in range(maxLength):
-        buy_list[j].showBuy()
-        sell_list[j].showSell()
+        if len(buy_list) > j:
+            buy_list[j].showBuy()
+
+        if len(sell_list) > j:
+            sell_list[j].showSell()
+def amend(buy_list, sell_list, oId,field,fldValue):
+    orderList = []
+    orderList = buy_list+sell_list
+
+    for i in orderList:
+        if i.ordId==oId:
+            if field == 'P':
+                i.Price=fldValue
+            elif field == 'Q':
+                i.Qty=fldValue
+            else:
+                print("Invalid Amend")
+
 def match(buy_list, sell_list):
+    flag = 0
     for i in buy_list:
         for j in sell_list:
             if i.Price == j.Price:
                 print("Order matching done")
+                flag = 1
                 buy_list.remove(i)
                 sell_list.remove(j)
                 break
-    else:
+    if flag == 0:
         print("Match did not happened.")
 
-
 if __name__ == '__main__':
-    n = int(input("Total no. of commands to enter:"))
+    #n = int(input("Total no. of commands to enter:"))
     print("This program supports action like: N(New), A(Amend), X(Cancel), M(Match) or Q(Query)")
-    print("New order Syntax: N,SYM,Side,Price, Qty")
+    print("New order Syntax: N,OrdId,SYM,Side,Price, Qty")
     buy_list = []
     sell_list = []
 
-    while n >0 :
-        lst1 = input().upper().split(',')
+    while True:
+        Str = input().upper().split(',')
 
-        if lst1[0] == 'N' and lst1[2] == 'B':
-            if lst1[1].isnumeric():
+        if Str[0] == 'N' and Str[3] == 'B':
+            if Str[2].isnumeric():
                 print('invalid Order')
             else:
-                buy_list.append(Order(lst1))
-        elif lst1[0] == 'N' and lst1[2] == 'S':
-            if lst1[1].isnumeric():
+                buy_list.append(Order(Str))
+        elif Str[0] == 'N' and Str[3] == 'S':
+            if Str[2].isnumeric():
                 print('invalid Order')
             else:
-                sell_list.append(Order(lst1))
-        elif lst1[0] =='Q':
+                sell_list.append(Order(Str))
+        elif Str[0] == 'A':
+            ordId=Str[1]
+            field=Str[2]
+            fldValue=Str[3]
+            amend(buy_list,sell_list,ordId,field,fldValue)
+
+        elif Str[0] == 'Q':
             query(buy_list, sell_list)
 
-        elif lst1[0] == 'M':
-            match(buy_list,sell_list)
+        elif Str[0] == 'M':
+            match(buy_list, sell_list)
+        else:
+            break
 
-        n -= 1
+
 
 
 
