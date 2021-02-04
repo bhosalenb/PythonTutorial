ae73322ec5b47947ce4cfe209a5039df84a26b85
nitinbhosale <bhosalenb@gmail.com>
2021-02-04 09:54:48 +0530

    Demo_Quick Fix

--- Demo_QuickFix/Client_Initiator.py
+++ Demo_QuickFix/Client_Initiator.py
@@ -1,14 +1,28 @@
 import quickfix
+import time
+import sys
+import quickfix42
 from Demo_QuickFix import MyApplication
 settings = quickfix.SessionSettings('client.cfg')
 application = MyApplication.MyApplication()
 storeFactory = quickfix.FileStoreFactory(settings)
 logFactory = quickfix.FileLogFactory(settings)
 
-initiator = quickfix.SocketInitiator(application,storeFactory,settings,logFactory)
+initiator = quickfix.SocketInitiator(application, storeFactory, settings, logFactory)
 initiator.start()
-application.run()
+#application.run()
+
+#while 1:
+time.sleep(2)
+
+if input() == '1':
+    print("Sending order: ")
+    application.send_order()
+if input() == '2':
+    sys.exit(0)
+
+else:
+    print("value input is 1 for order, 2 for exit")
+
 
 initiator.stop()
-
-
--- Demo_QuickFix/MyApplication.py
+++ Demo_QuickFix/MyApplication.py
@@ -1,32 +1,75 @@
 
 import quickfix
+#from model.Message import Base, Types, __SOH__
 import time
 
 class MyApplication(quickfix.Application):
     sessionID = None
     def onCreate(self, sessionID):
+        print("on Create")
         return
 
     def onLogon(self, sessionID):
         self.sessionID = sessionID
+        print("Successful Logon to session '%s'." % sessionID.toString())
         return
 
     def onLogout(self,sessionID):
+        print("logged out")
         return
 
 
     def toAdmin(self,message,sessionID):
-        msg = message.toString().replace(__SOH__, "|")
+        pass
+        #msg = message.toString().replace(__SOH__, "|")
+
 
     def fromAdmin(self,message,sessionID):
-        msg = message.toString().replace(__SOH__, "|")
+        pass
+        #msg = message.toString().replace(__SOH__, "|")
 
     def toApp(self,message,sessionID):
-        msg = message.toString().replace(__SOH__, "|")
+        pass
+        #msg = message.toString().replace(__SOH__, "|")
 
     def fromApp(self,message,sessionID):
-        msg = message.toString().replace(__SOH__, "|")
+        beginString = quickfix.BeginString()
+        msgType = quickfix.MsgType()
+
+        message.getHeader().getField(beginString)
+        message.getHeader().getField(msgType)
+
+        symbol = quickfix.Symbol()
+        side = quickfix.Side()
+        ordType = quickfix.OrdType()
+        orderQty = quickfix.OrderQty()
+        price = quickfix.Price()
+        clOrdID = quickfix.ClOrdID()
+        message.getField(ordType)
+        print("Order Type: ", ordType)
+        print("Received Application message: ")
+        #msg = message.toString().replace(__SOH__, "|")
 
     def run(self):
         while 1:
             time.sleep(2)
+    def send_order(self):
+        print("Creating the following order: ")
+
+        order = quickfix.Message()
+
+        #order.getHeader().setField(quickfix.BeginString(quickfix.BeginString_FIX42))
+        order.getHeader().setField(quickfix.MsgType(quickfix.MsgType_NewOrderSingle))
+
+        order.setField(quickfix.ClOrdID('123'))
+        order.setField(quickfix.HandlInst('3'))
+        order.setField(quickfix.Symbol("TCS"))
+        order.setField(quickfix.Side(quickfix.Side_BUY))
+        order.setField(quickfix.TransactTime())
+        order.setField(quickfix.OrderQty(1001))
+        order.setField(quickfix.OrdType(1))
+
+        print(order.toString())
+        print(self.sessionID)
+
+        quickfix.Session.sendToTarget(order, self.sessionID)
